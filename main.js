bd97d8fe83e2fb2f496add928a32d568c48e14ce
martinachondo <acho.martin@gmail.com>
2022-11-01 00:06:30 -0400

    app

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
-const fs = require('fs');
 const functions = require('./window/functions')
 
 let mainWindow;
